b12a8c108400a90bf241e294e5fd42572502aa23
HarryKim <hangin359@gmail.com>
2018-12-04 19:19:55 +0900

    template engine: jade

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
 
+app.locals.pretty = true;
+
+// express가 template를 렌더링하기 위해 필요한 설정
+app.set('view engine', 'jade');
+app.set('views', './views');
+
+// public이라는 경로에 정적 파일이 위치하도록 지정
+app.use(express.static('public'));
+
 // Routing(get) & Controller(res.send)
 app.get('/', function(req, res){
 	res.send('Hello World');
@@ -8,6 +17,15 @@ app.get('/', function(req, res){
 );
 app.get('/login', function(req, res){
 	res.send('<h1>Login please</h1>');
+}
+);
+app.get('/route', function(req, res){
+	res.send('<img src="/static_image.jpg">');
+}
+);
+
+app.get('/template', function(req, res){
+	res.render('temp', {time: Date(), _title: 'Jade'});
 }
 );
 
