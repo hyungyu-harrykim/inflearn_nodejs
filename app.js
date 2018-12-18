f06cd169407b6996aa72392d92fb42cb9816442a
HarryKim <hangin359@gmail.com>
2018-12-18 13:32:57 +0900

    query string

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.set('views', './views');
 // public이라는 경로에 정적 파일이 위치하도록 지정
 app.use(express.static('public'));
 
+
 // Routing(get) & Controller(res.send)
 app.get('/', function(req, res){
 	res.send('Hello World');
@@ -29,6 +30,21 @@ app.get('/template', function(req, res){
 }
 );
 
+app.get('/topic', function(req, res){
+	var topics = [
+		'Javascript is ...',
+		'Nodejs is ...',
+		'Express is...'
+	];
+	var output = `
+	<a href="/topic?id=0">Javascript</a><br>
+	<a href="/topic?id=1">Nodejs</a><br>
+	<a href="/topic?id=2">Express</a><br><br>
+	${topics[req.query.id]}
+	`
+	res.send(output);	// query string
+}
+);
 
 app.listen(3000, function(){
 	console.log('Connected 3000 port');
