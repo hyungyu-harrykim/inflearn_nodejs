63f11d8cd0d375bab5d626e82a42806f4ad0bdf9
HarryKim <hangin359@gmail.com>
2018-12-26 19:44:57 +0900

    saving file

--- app.js
+++ app.js
@@ -41,9 +41,9 @@ app.get('/topic/:id', function(req, res){
 		'Express is...'
 	];
 	var output = `
-	<a href="/topic?id=0">Javascript</a><br>
-	<a href="/topic?id=1">Nodejs</a><br>
-	<a href="/topic?id=2">Express</a><br><br>
+	<a href="/topic/0">Javascript</a><br>
+	<a href="/topic/1">Nodejs</a><br>
+	<a href="/topic/2">Express</a><br><br>
 	${topics[req.params.id]}
 	`
 	res.send(output);	// query string
